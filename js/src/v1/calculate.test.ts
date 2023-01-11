ebf72620bb32481237046d64f0f0a6601b5cdfce
Gert de Pagter <gert.de.pagter@yoursurprise.com>
2023-01-11 09:43:21 +0100

    Add it.each and an example to try themselves

--- js/src/v1/calculate.test.ts
+++ js/src/v1/calculate.test.ts
@@ -4,4 +4,17 @@ describe('calculate v1', () => {
     it('adds two numbers', () => {
         expect(add(2,4)).toBe(6);
     })
+
+    it.each([
+        [1, 2, 3],
+        [0, 0, 0],
+        [-1, 6, 5],
+        [4, -2, 2],
+        [1, -7, -6],
+        [-3, 2, -1],
+        [-5, -4, -9]
+    ])('adds numbers', (one, two, result) => {
+
+        expect(add(one, two)).toBe(result)
+    })
 });
